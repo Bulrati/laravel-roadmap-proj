11a81a1fd68dd90f7c0be36b405ba66ae5f26166
serhii nastenko <serhii.nastenko@sigma.software>
2019-12-01 01:20:05 +0200

    Seeders were added.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -16,12 +17,20 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+$factory->define( \App\User::class, function ( Faker $faker ) {
+	return [
+		'name'  => $faker->name,
+		'email' => $faker->unique()->email,
+	];
+} );
+
+$factory->define( \App\Post::class, function ( Faker $faker ) {
+	return [
+		'author_id' => $faker->numberBetween( 1, 2 ),
+		'slug'      => $faker->unique()->word,
+		'title'     => $faker->sentence,
+		'content'   => $faker->sentence,
+		'excerpt'   => $faker->sentence,
+		'status_id' => $faker->numberBetween( 1, 3 )
+	];
+} );
---
+++ database/seeds/PostStatusesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostStatusesTableSeeder extends Seeder {
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run() {
+
+		\Illuminate\Support\Facades\DB::table( 'post_statuses' )->insert( [
+			'status' => 'published'
+		] );
+		\Illuminate\Support\Facades\DB::table( 'post_statuses' )->insert( [
+			'status' => 'unpublished'
+		] );
+		\Illuminate\Support\Facades\DB::table( 'post_statuses' )->insert( [
+			'status' => 'draft'
+		] );
+	}
+}
