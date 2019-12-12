5820ab3b4a24065c491decac1494fd2c872622ae
serhii nastenko <serhii.nastenko@sigma.software>
2019-12-12 10:42:33 +0200

    Footer was changed.

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,3 +1,5 @@
-<footer class="text-muted">
-
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">@ Mr. John Loncaster Peck</span>
+    </div>
 </footer>
--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -10,7 +10,11 @@
 
 <!-- Bootstrap core CSS -->
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://getbootstrap.com/docs/4.1/examples/sticky-footer/sticky-footer.css">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 <!-- Custom styles for this template -->
 <link href="/css/album.css" rel="stylesheet">
