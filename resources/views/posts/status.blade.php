a54cc578744d36325f031a3ab224408aaa6cc7ab
serhii nastenko <serhii.nastenko@sigma.software>
2020-03-22 17:59:22 +0200

    Flash messages were added

--- resources/views/posts/status.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.mainlayout')
-@section('content')
-    <div class="container">
-        <p>{{ $status }}</p>
-        <a href="{{route('post.index')}}" class="btn btn-primary">Back to the all posts</a>
-    </div>
-@endsection
